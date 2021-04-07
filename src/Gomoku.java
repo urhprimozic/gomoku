ee7cfade3dc5813bda9cded39af01b49ca1ce9ef
Jon Mikoš <atletjm@gmail.com>
2021-04-07 13:31:24 +0200

    test loop update

--- src/Gomoku.java
+++ src/Gomoku.java
@@ -9,22 +9,37 @@ public class Gomoku {
     public static void main(String[] args) {
     	
     	Scanner in = new Scanner(System.in);
-    	
+    	game_loop:
         while (true) {
-        	Igra igra = new Igra();
+        	Igra igra = new Igra(5, 5);
         	
-        	igra.odigrajNakljucnoPotezo();
-        	System.out.println(igra);
+        	int i = 0;
         	
-        	if (igra.stanje() != Stanje.V_TEKU) {
-        		System.out.println("Ponovi igro? [D/N]");
-        		String s = in.nextLine();
-        		
-        		if (s.equals("N")) {
-        			break;
-        		}
-        	}        	
+        	inner_loop:
+        	while (true) {
+        		++i;
+        		igra.odigrajNakljucnoPotezo();
+            	System.out.println(igra);
+            	
+            	System.out.print(i + " ");
+            	System.out.println(igra.stanje());
+            	splosno.Koordinati zadnja = igra.odigranePoteze.getLast().getKoordinati();
+            	System.out.println("(" + zadnja.getX() + ", " + zadnja.getY() + ") " + igra.naPotezi.nasprotnik());
+            	System.out.println("---------------------------------------------");
+            	if (igra.stanje() != Stanje.V_TEKU) {
+            		System.out.println("Ponovi igro? [D/N]");
+            		String s = in.nextLine();
+            		
+            		if (s.equals("N")) {
+            			break game_loop;
+            		}
+            		else {
+            			break inner_loop;
+            		}
+            	}        	
+        	}       	
         }
+        in.close();
                 
     }
 }
