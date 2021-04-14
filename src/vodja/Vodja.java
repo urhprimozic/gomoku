1aa142b18f8af3d7cbbecfeb6b655df3dc9d1130
urh <urh.primozic@gmail.com>
2021-04-14 09:02:12 +0200

    primitivni vodja

--- src/vodja/Vodja.java
+++ src/vodja/Vodja.java
@@ -28,39 +28,39 @@ public class Vodja {
 	}
 	
 	public static void igramo () {
-		//okno.osveziGUI();
-		//switch (igra.stanje()) {
-		//case ZMAGA_O: 
-		//case ZMAGA_X: 
-		//case NEODLOCENO: 
-		//	return; // odhajamo iz metode igramo
-		//case V_TEKU: 
-		//	Igralec igralec = igra.naPotezi;
-		//	VrstaIgralca vrstaNaPotezi = vrstaIgralca.get(igralec);
-		//	switch (vrstaNaPotezi) {
-		//	case C: 
-		//		clovekNaVrsti = true;
-		//		break;
-		//	case R:
-		//		igrajRacunalnikovoPotezo ();
-		//		break;
-		//	}
-		// }
+		okno.osveziGUI();
+		switch (igra.dobiNovoStanje()) {
+		case ZMAGA_B: 
+		case ZMAGA_C: 
+		case NEODLOCENO: 
+			return; // odhajamo iz metode igramo
+		case V_TEKU: 
+			Igralec igralec = igra.naPotezi;
+			VrstaIgralca vrstaNaPotezi = vrstaIgralca.get(igralec);
+			switch (vrstaNaPotezi) {
+			case C: 
+				clovekNaVrsti = true;
+				break;
+			case R:
+				igrajRacunalnikovoPotezo ();
+				break;
+			}
+		 }
 	}
 	
 	private static Random random = new Random ();
 	
-//	public static void igrajRacunalnikovoPotezo() {
-//		List<Koordinati> moznePoteze = igra.poteze();
-//		try {TimeUnit.SECONDS.sleep(2);} catch (Exception e) {};
-//		int randomIndex = random.nextInt(moznePoteze.size());
-//		Koordinati poteza = moznePoteze.get(randomIndex);
-//		igra.odigraj(poteza);
-//		igramo ();
-//	}
-	
 	public static void igrajRacunalnikovoPotezo() {
-		Igra zacetkaIgra = igra;
+		List<Koordinati> moznePoteze = igra.moznePoteze();
+		try {TimeUnit.SECONDS.sleep(1);} catch (Exception e) {};
+		int randomIndex = random.nextInt(moznePoteze.size());
+		Koordinati poteza = moznePoteze.get(randomIndex);
+		igra.odigraj(poteza);
+		igramo ();
+	}
+	
+	//public static void igrajRacunalnikovoPotezo() {
+	//	Igra zacetkaIgra = igra;
 		// SwingWorker<Koordinati, Void> worker = new SwingWorker<Koordinati, Void> () {
 		// 	@Override
 		// 	protected Koordinati doInBackground() {
@@ -80,7 +80,7 @@ public class Vodja {
 		// 	}
 		// };
 		// worker.execute();
-	}
+	//}
 		
 	public static void igrajClovekovoPotezo(Koordinati poteza) {
 		if (igra.odigraj(poteza)) clovekNaVrsti = false;
