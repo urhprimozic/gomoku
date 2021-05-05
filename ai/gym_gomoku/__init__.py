efd7468547de2c6eb62e3f83dd83e336bf9949af
urh <urh.primozic@gmail.com>
2021-05-05 10:19:07 +0200

    nek weird ass gomoku implementation

---
+++ ai/gym_gomoku/__init__.py
@@ -0,0 +1,35 @@
+from gym.envs.registration import register
+
+register(
+    id='Gomoku19x19-v0',
+    entry_point='gym_gomoku.envs:GomokuEnv',
+    kwargs={
+        'player_color': 'black',
+        'opponent': 'beginner', # beginner opponent policy has defend and strike rules
+        'board_size': 19,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Gomoku15x15-NN-monteCarlo',
+    entry_point='gym_gomoku.envs:GomokuEnv',
+    kwargs={
+        'player_color': 'black',
+        'opponent': 'neural_net', # beginner opponent policy has defend and strike rules
+        'board_size': 15,
+    },
+    nondeterministic=True,
+)
+
+register(
+    id='Gomoku9x9-v0',
+    entry_point='gym_gomoku.envs:GomokuEnv',
+    kwargs={
+        'player_color': 'black',
+        'opponent': 'beginner', # random policy is the simplest
+        'board_size': 9,
+    },
+    nondeterministic=True,
+)
+
